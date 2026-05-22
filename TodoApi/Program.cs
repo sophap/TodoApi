4547cf8e774d0d4f9e0eebc848dcc524fe16c61e
PRAISE SOPHIA RIGHT <rightpraise@gmail.com>
2026-05-22 18:04:20 +0100

    Moved all routes in Program.cs to TodoRoutes.cs

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using TodoApi.Data;
 using TodoApi.Models;
 using Scalar.AspNetCore;
+using TodoApi.Routes;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,56 +42,6 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-// Routes
-var todos = app.MapGroup("/todos");
-
-// GET all
-todos.MapGet("/", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
-
-// GET by id
-todos.MapGet("/{id:int}", async (int id, TodoDb db) =>
-{
-    var todo = await db.Todos.FindAsync(id);
-    return todo is not null ? Results.Ok(todo) : Results.NotFound();
-});
-
-// CREATE
-todos.MapPost("/", async (Todo todo, TodoDb db) =>
-{
-    if (string.IsNullOrWhiteSpace(todo.Title))
-        return Results.BadRequest("Title is required");
-
-    db.Todos.Add(todo);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/todos/{todo.Id}", todo);
-});
-
-// UPDATE
-todos.MapPut("/{id:int}", async (int id, Todo updated, TodoDb db) =>
-{
-    var todo = await db.Todos.FindAsync(id);
-    if (todo is null) return Results.NotFound();
-
-    todo.Title = updated.Title;
-    todo.IsCompleted = updated.IsCompleted;
-
-    await db.SaveChangesAsync();
-    return Results.Ok(todo);
-});
-
-// DELETE
-todos.MapDelete("/{id:int}", async (int id, TodoDb db) =>
-{
-    var todo = await db.Todos.FindAsync(id);
-    if (todo is null) return Results.NotFound();
-
-    db.Todos.Remove(todo);
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
+app.MapTodoRoutes();
 
 app.Run();
-
---
+++ TodoApi/Routes/TodoRoutes.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Routes;
+
+public static class TodoRoutes
+{
+    public static void MapTodoRoutes(this WebApplication app)
+    {
+        // Routes
+        var todos = app.MapGroup("/todos");
+
+        // GET all
+        todos.MapGet("/", async (TodoDb db) =>
+            await db.Todos.ToListAsync());
+
+        // GET by id
+        todos.MapGet("/{id:int}", async (int id, TodoDb db) =>
+        {
+            var todo = await db.Todos.FindAsync(id);
+            return todo is not null ? Results.Ok(todo) : Results.NotFound();
+        });
+
+        // CREATE
+        todos.MapPost("/", async (Todo todo, TodoDb db) =>
+        {
+            if (string.IsNullOrWhiteSpace(todo.Title))
+                return Results.BadRequest("Title is required");
+
+            db.Todos.Add(todo);
+            await db.SaveChangesAsync();
+
+            return Results.Created($"/todos/{todo.Id}", todo);
+        });
+
+        // UPDATE
+        todos.MapPut("/{id:int}", async (int id, Todo updated, TodoDb db) =>
+        {
+            var todo = await db.Todos.FindAsync(id);
+            if (todo is null) return Results.NotFound();
+
+            todo.Title = updated.Title;
+            todo.IsCompleted = updated.IsCompleted;
+
+            await db.SaveChangesAsync();
+            return Results.Ok(todo);
+        });
+
+        // DELETE
+        todos.MapDelete("/{id:int}", async (int id, TodoDb db) =>
+        {
+            var todo = await db.Todos.FindAsync(id);
+            if (todo is null) return Results.NotFound();
+
+            db.Todos.Remove(todo);
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        });
+    }
+}
